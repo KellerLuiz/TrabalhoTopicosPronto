87593f47f53b3f91686fad65a1646d3f48145cd5
KellerLuiz <luizkeller2@gmail.com>
2019-05-27 13:47:20 -0300

    versão com auth

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/mensagens', 'MensagensController@index');
-Route::get('/mensagens/create', 'MensagensController@create');
-Route::post('/mensagens', 'MensagensController@store');
-Route::get('/mensagens/{id}', 'MensagensController@show');
-Route::get('/mensagens/{id}/edit', 'MensagensController@edit');
-Route::put('/mensagens/{id}', 'MensagensController@update');
-Route::get('/mensagens/{id}/delete','MensagensController@delete');
-Route::delete('/mensagens/{id}', 'MensagensController@destroy');
+
 
 
 Route::get('/atividades', 'AtividadeController@index');
-Route::get('/atividades/create', 'AtividadeController@create');
-Route::post('/atividades', 'AtividadeController@store');
-Route::get('/atividades/{id}', 'AtividadeController@show');
-Route::get('/atividades/{id}/edit', 'AtividadeController@edit');
-Route::put('/atividades/{id}', 'AtividadeController@update');
-Route::get('/atividades/{id}/delete','AtividadeController@delete');
-Route::delete('/atividades/{id}', 'AtividadeController@destroy');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/mensagens/create', 'MensagensController@create');
+    Route::post('/mensagens', 'MensagensController@store');
+    Route::get('/mensagens/{id}', 'MensagensController@show');
+    Route::get('/mensagens/{id}/edit', 'MensagensController@edit');
+    Route::put('/mensagens/{id}', 'MensagensController@update');
+    Route::get('/mensagens/{id}/delete','MensagensController@delete');
+    Route::delete('/mensagens/{id}', 'MensagensController@destroy');
+
+    Route::get('/atividades/create', 'AtividadeController@create');
+    Route::post('/atividades', 'AtividadeController@store');
+    Route::get('/atividades/{id}', 'AtividadeController@show');
+    Route::get('/atividades/{id}/edit', 'AtividadeController@edit');
+    Route::put('/atividades/{id}', 'AtividadeController@update');
+    Route::get('/atividades/{id}/delete','AtividadeController@delete');
+    Route::delete('/atividades/{id}', 'AtividadeController@destroy');
+});
+
+
+Route::prefix('admin')->middleware('auth')->namespace('Admin')->group(function(){
+    
+
+});
